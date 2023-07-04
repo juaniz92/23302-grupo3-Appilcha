00385145d74090303fd6a303689940a23535a865
sebastoss <sfpozzi@gmail.com>
2023-07-04 15:46:00 -0300

    Se añade función para descargar factura con html2canvas.

--- src/componentes/CarritoTotal.jsx
+++ src/componentes/CarritoTotal.jsx
@@ -6,7 +6,7 @@ const CarritoTotal = () => {
 
     const total = carrito.reduce((acc,el) => acc + el.price * el.cantidad, 0);
     return <div className='ps-2 pb-3 mt-2'>
-        <h3><span className='fw-bold fs-4'>Total a pagar:</span>  $ {parseFloat(total.toFixed(2))}</h3>
+        <h3><span className='fw-bold fs-4' id='totalPagar'>Total a pagar:</span>  $ {parseFloat(total.toFixed(2))}</h3>
     </div>
 }
 
