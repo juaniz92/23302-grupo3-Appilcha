49e790432168cb6189f137d3d57bc3e56b3ac1f1
sebastoss <sfpozzi@gmail.com>
2023-07-06 20:34:40 -0300

    Agrego funcion para plegar el menu hamburguesa al pasar X segs

--- src/componentes/Header.jsx
+++ src/componentes/Header.jsx
@@ -4,9 +4,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
+
 function Header() {
+  
+  
+  // Función para cerrar el menú hamburguesa al pasar X segundos
+  
+  const handleTogglerNav = e =>{
+    
+    if((e.target.nodeName === "BUTTON" && e.target.classList.contains("collapsed")) || (e.target.nodeName === "SPAN" && e.target.parentElement.classList.contains("collapsed"))){  
+      setTimeout(()=>{
+        e.target.click();
+      }, 4000);
+    }
+  }
+  
 
 return (
+
     //Navbar de react-boostrap
     <Navbar expand="lg" className="light">
       <Container>
@@ -16,13 +31,11 @@ return (
               className="d-inline-block align-top"
               alt="Appilcha Logo"
             /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle onClick={e => handleTogglerNav(e)} aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto text-uppercase">
-            <Nav.Link href="/">Inicio</Nav.Link>
-            <Nav.Link href="/Tienda">Tienda</Nav.Link>
-            
-            
+            <Nav.Link as={Link} to="/">Inicio</Nav.Link>
+            <Nav.Link as={Link} to="/Tienda">Tienda</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
